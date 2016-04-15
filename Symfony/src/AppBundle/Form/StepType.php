4dc73945939a1c1947a9ae12663d9ac3bfdaaa73
aluok <humbert.timede@gmail.com>
2016-04-15 06:30:40 +0530

    Improve view of form for new recipe steps

--- Symfony/src/AppBundle/Form/StepType.php
+++ Symfony/src/AppBundle/Form/StepType.php
@@ -5,6 +5,8 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class StepType extends AbstractType
 {
@@ -15,8 +17,8 @@ class StepType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('text')
-            ->add('image')
+            ->add('text', TextareaType::class)
+            ->add('image', FileType::class)
         ;
     }
 
