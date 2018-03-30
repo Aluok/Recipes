a0fb800e53ce2618b55080018b051b3e0c7131a1
Julien <julien@afrique-telecom.com>
2018-03-30 12:31:35 +0200

    Update to 3.3

--- Symfony/src/UserBundle/Controller/RegistrationController.php
+++ Symfony/src/UserBundle/Controller/RegistrationController.php
@@ -1,13 +1,26 @@
 <?php
 namespace UserBundle\Controller;
 
+use FOS\UserBundle\Form\Factory\FactoryInterface;
+use FOS\UserBundle\Model\UserManagerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use FOS\UserBundle\Controller\RegistrationController as BaseController;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class RegistrationController extends BaseController
 {
+    public function __construct(
+        EventDispatcherInterface $eventDispatcher,
+        FactoryInterface $formFactory,
+        UserManagerInterface $userManager,
+        TokenStorageInterface $tokenStorage
+    )
+    {
+        parent::__construct($eventDispatcher, $formFactory, $userManager, $tokenStorage);
+    }
 
-    public function confirmedAction()
+    public function confirmedAction(Request $request)
     {
         $this->addFlash('notice', 'Inscription confirmed. You are now connected');
         return $this->redirectToRoute('recipe_home');
